165d0fb292f4d3bcf8f8ea7211937ec53da67841
Ishaan Sarna <57299072+ishaansarna@users.noreply.github.com>
2022-10-08 12:07:38 +0530

    Time: 305 ms (7.96%), Space: 18.6 MB (31.01%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,19 +1,16 @@
 class Solution {
     int sum, target;
     vector<int> cands;
-    vector<vector<int>> ans;
+    set<vector<int>> ans;
     void backtrack(vector<int>& candidates) {
         sum = accumulate(cands.begin(), cands.end(), 0);
         if (sum == target) {
-            // sort(cands.begin(), cands.end());
-            ans.push_back(cands);
+            vector<int> x (cands);
+            sort(x.begin(), x.end());
+            ans.insert(x);
         }
         for (int c : candidates) {
             sum = accumulate(cands.begin(), cands.end(), 0);
-            // if (cands.size() == 0 && c == 3)
-            //     cout << "hi\n";
-            // if (c == 3)
-            //     cout << "yo";
             if (c + sum <= target) {
                 cands.push_back(c);
                 backtrack(candidates);
@@ -26,11 +23,7 @@ public:
         sum = 0;
         this->target = target;
         backtrack(candidates);
-        for (int i = 0; i<ans.size(); i++)
-            sort(ans[i].begin(), ans[i].end());
-        set<vector<int>> res;
-        for (auto i : ans)
-            res.insert(i);
-        return vector<vector<int>> (res.begin(), res.end());
+        vector<vector<int>> res(ans.begin(), ans.end());
+        return res;
     }
 };
